667fca61b19ab5e25f56f7cc075c8de3233b1ad1
Delice Lydia <Delydia84@gmail.com>
2021-08-12 11:02:22 +0200

    Delete unnecessary migration

--- db/migrate/20210806111148_add_user_to_reservations.rb
+++
@@ -1,5 +0,0 @@
-class AddUserToReservations < ActiveRecord::Migration[6.1]
-  def change
-    add_reference :reservations, :user, null: false, foreign_key: true
-  end
-end
--- db/migrate/20210806111323_add_teacher_to_reservations.rb
+++
@@ -1,5 +0,0 @@
-class AddTeacherToReservations < ActiveRecord::Migration[6.1]
-  def change
-    add_reference :reservations, :teacher, null: false, foreign_key: true
-  end
-end
