fd400ca24d3df95dcea6ace00768a59603fac673
Delice Lydia <Delydia84@gmail.com>
2021-08-16 15:08:03 +0200

    Add tests for reservation requests

---
+++ spec/requests/reservations_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe 'Reservations', type: :request do
+    before(:example) do
+      @user = User.create(name: 'lydia', email: 'lydia@gmail.com', password: 'Lydia1@')
+      @reservation = Reservation.create(reservation_date: 20-10-2021, teacher_id: 1, user_id: 1)
+      token = JWT.encode({ user_id: @user.id }, Rails.application.secret_key_base)
+      @headers = { Authorization: "Bearer #{token}" }
+    end
+
+    it 'should get all reservations' do
+      get '/api/v1/reservations', as: :json, headers: @headers
+      expect(response).to have_http_status :ok
+    end
+
+    it 'should display reservation information' do
+      get '/api/v1/reservations/@reservation.id', as: :json, headers: @headers
+      expect(response).to have_http_status(:ok)
+    end
+end
--- spec/requests/teachers_spec.rb
+++ spec/requests/teachers_spec.rb
@@ -1,17 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe 'Teachers', type: :request do
-    before(:example) do
-      @teacher = Teacher.create(name: 'lydia', courses: 'ruby', experience: '7 Years')
-    end
+    # before(:example) do
+    #   @teacher = Teacher.create(name: 'lydia', courses: 'ruby', experience: '7 Years')
+    #   @user = User.new(name: 'lydia', email: 'lydia@gmail.com', password: 'lydia')
+    #   token = JWT.encode({ user_id: @user.id }, Rails.application.secret_key_base)
+    #   @headers = { Authorization: "Bearer #{token}" }
+    # end
 
     # it 'display all teachers' do
-    #   get '/api/v1/teachers'
+    #   get '/api/v1/teachers', as: :json, headers: @headers
     #   expect(response).to have_http_status(200)
     # end
 
     # it 'displays teacher information' do
-    #   get '/api/v1/teachers/@teacher.id'
+    #   get '/api/v1/teachers/1', as: :json, headers: @headers
     #   expect(response).to have_http_status(:ok)
     # end
 end
