d0f44dd014b4c0396e5398b2e7dce30b91564bed
delicelydia <Delydia84@gmail.com>
2021-09-29 23:31:05 +0200

    Add authentication

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -1,11 +1,11 @@
 class AuthenticationsController < ApplicationController
-  # skip_before_action :authorize_request, only: :create
+  skip_before_action :authorize_request, only: :create
 
   def create
     user = User.find_by(email: params[:email])
     auth_token =
       AuthenticateUser.new(auth_params[:name], auth_params[:email], auth_params[:password]).call
-      render json: { name: user.name, email: user.email }
+      render json: { name: user.name, email: user.email, auth_token: auth_token }
   end
 
   private
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,10 @@
 class UsersController < ApplicationController
-  # skip_before_action :authorize_request, only: :create
+  skip_before_action :authorize_request, only: :create
 
   def create
     user = User.create!(user_params)
-    # auth_token = AuthenticateUser.new(user.email, user.password).call
-    res = { message: Message.account_created}
+    auth_token = AuthenticateUser.new(user.name, user.email, user.password).call
+    res = { message: Message.account_created, auth_token: auth_token}
     json_response(res, :created)
   end
 
