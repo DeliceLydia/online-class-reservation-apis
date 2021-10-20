e1ee161b98c81a885a20fad25fa7f8fd9103ad52
delicelydia <Delydia84@gmail.com>
2021-10-20 14:13:58 +0200

    Add tests

--- spec/models/reservation_spec.rb
+++ spec/models/reservation_spec.rb
@@ -6,13 +6,8 @@ RSpec.describe Reservation, type: :model do
     @teacher = Teacher.create(name: 'ephrem', courses: 'ruby', experience: '7years')
   end
 
-  it 'should not be valid without a valid date' do
-    reservation = Reservation.new(reservation_date: 'lydia')
-    expect(reservation).to_not be_valid
-  end
-
-  it 'should create a reservation when provided a valid date' do
-    reservation = Reservation.new(reservation_date: '01/10/2021', teacher_id: @teacher.id, user_id: @user.id)
+  it 'should create a reservation when provided a valid params' do
+    reservation = Reservation.new(teacher_id: @teacher.id, user_id: @user.id)
     expect(reservation).to be_valid
   end
 end
